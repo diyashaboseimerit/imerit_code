643f8fa9833982eb7f58a9174d2017b01ca62830
diyashaboseimerit <diyasha.b@imeritconsultant.net>
2025-08-18 19:18:11 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,10 +1,16 @@
 const { fetchAll } = require('./controller/announcementController');
+const {fetchPController}=require("./controller/progressController");
 
 const routes = [
   {
     method: 'POST',
     path: '/announcement',
     handler: fetchAll
+  },
+  {
+    method: 'POST',
+    path: '/checkProgress',
+    handler: fetchPController
   }
 ];
 
